a7e4c0437d78106fd4a5ca71f81d32801b6c7b44
VICTOR COLORADO GAJARDO <kidacolorado@gmail.com>
2023-09-19 13:02:00 +0200

    segundo commit

--- src/main/java/com/viewnext/kidaprojects/agenciaviajes/AgenciaViajesApplication.java
+++ src/main/java/com/viewnext/kidaprojects/agenciaviajes/AgenciaViajesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgenciaViajesApplication {
-
+	//prueba para git
 	public static void main(String[] args) {
 		SpringApplication.run(AgenciaViajesApplication.class, args);
 	}
