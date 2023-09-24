a85b7fac3a7838f0e5c25cd6fd00dec1cace4e19
VICTOR COLORADO GAJARDO <kidacolorado@gmail.com>
2023-09-25 00:04:38 +0200

    ultimo commit del finde. Añadidos readme, tests

--- src/test/java/com/viewnext/kidaprojects/agenciaviajes/AgenciaViajesApplicationTests.java
+++ src/test/java/com/viewnext/kidaprojects/agenciaviajes/AgenciaViajesApplicationTests.java
@@ -1,13 +1,68 @@
 package com.viewnext.kidaprojects.agenciaviajes;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static io.restassured.RestAssured.given;
+import com.viewnext.kidaprojects.agenciaviajes.model.Pasajero;
+import com.viewnext.kidaprojects.agenciaviajes.repository.PasajeroRepository;
+import com.viewnext.kidaprojects.agenciaviajes.service.PasajeroService;
+import com.viewnext.kidaprojects.agenciaviajes.viewcontrollers.PasajeroViewController;
 
-@SpringBootTest
+
+@DataJpaTest
 class AgenciaViajesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private PasajeroViewController pasajeroController;
 
+    @Autowired
+    private PasajeroService pasajeroService;
+
+    @Autowired
+    private PasajeroRepository pasajeroRepository;
+
+    @Test
+    void contextLoads() {
+        assertThat(pasajeroController).isNotNull();
+    }
+
+    @Test
+    void testBuscarPasajeroPorId() {
+        Optional<Pasajero> optionalPasajero = pasajeroService.findById(1); // Reemplaza con un ID existente
+        if (optionalPasajero.isPresent()) {
+        	Pasajero pasajero = optionalPasajero.get();
+        	assertEquals("Juan", pasajero.getNombre()); // Verifica si el nombre es el esperado
+        }
+        
+    }
+
+    @Test
+    void testObtenerPasajeroPorId() {
+        given()
+            .when()
+            .get("/api/pasajero/1") // Reemplaza con la ruta real del endpoint
+            .then()
+            .statusCode(200) // Verifica el código de respuesta
+            .body("nombre", equalTo("Juan")); // Verifica el contenido esperado
+    }
+
+    @Test
+    void testGuardarYRecuperarPasajero() {
+        Pasajero pasajero = new Pasajero();
+        pasajero.setNombre("Ashitaka");
+
+        pasajeroRepository.save(pasajero);
+        Pasajero recuperado = pasajeroRepository.findById(pasajero.getIdPasajero()).orElse(null);
+
+        assertEquals("Ashitaka", recuperado.getNombre());
+    }
+
+    
 }
+
